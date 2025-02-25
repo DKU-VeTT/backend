f2d575579f97bc044ffbbcb98c5d11c4faad89a6
ohchanKyu <okc0202@naver.com>
2025-02-25 17:04:47 +0900

    chore: Add admin place uri

--- VettAdminServer/src/main/java/kr/ac/dankook/VettAdminServer/restController/AdminPlaceRestController.java
+++ VettAdminServer/src/main/java/kr/ac/dankook/VettAdminServer/restController/AdminPlaceRestController.java
@@ -1,4 +1,21 @@
 package kr.ac.dankook.VettAdminServer.restController;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
+
+@RestController
+@RequestMapping("/admin/place")
+@RequiredArgsConstructor
 public class AdminPlaceRestController {
+
+    private final RestClient restClient;
+
+    @Value("${admin.header.name}")
+    private String ADMIN_HEADER_NAME;
+    @Value("${admin.gateway.server-url}")
+    private String ADMIN_GATEWAY_SERVER_URL;
+
 }
