36630f8d743c1bd0498a3504403011c0be340e78
ohchanKyu <okc0202@naver.com>
2025-03-02 13:16:01 +0900

    fix: fix jquery policy

--- VettAdminServer/src/main/java/kr/ac/dankook/VettAdminServer/restController/AdminPlaceRestController.java
+++ VettAdminServer/src/main/java/kr/ac/dankook/VettAdminServer/restController/AdminPlaceRestController.java
@@ -2,7 +2,6 @@ package kr.ac.dankook.VettAdminServer.restController;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClient;
@@ -16,6 +15,6 @@ public class AdminPlaceRestController {
 
     @Value("${admin.header.name}")
     private String ADMIN_HEADER_NAME;
-//    @Value("${admin.gateway.server-url}")
-//    private String ADMIN_GATEWAY_SERVER_URL;
+    @Value("${admin.gateway.server-url}")
+    private String ADMIN_GATEWAY_SERVER_URL;
 }
