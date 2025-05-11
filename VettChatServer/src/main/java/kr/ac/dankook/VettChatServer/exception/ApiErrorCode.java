2f3db37ea7cad82540cca72faa5eef1d7809272b
ohchanKyu <okc0202@naver.com>
2025-05-12 08:59:04 +0900

    remove: Remove not using error code.

--- VettChatServer/src/main/java/kr/ac/dankook/VettChatServer/exception/ApiErrorCode.java
+++ VettChatServer/src/main/java/kr/ac/dankook/VettChatServer/exception/ApiErrorCode.java
@@ -12,10 +12,6 @@ public enum ApiErrorCode implements ErrorCode{
     CHATROOM_NOT_FOUND(HttpStatus.NOT_FOUND,"Not found ChatRoom with your primary key."),
     KAFKA_SEND_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR,"Error during sending data to Kafka"),
     INVALID_REQUEST(HttpStatus.BAD_REQUEST, "Invalid request parameters."),
-    DUPLICATE_ID(HttpStatus.INTERNAL_SERVER_ERROR, "This is duplicated Id."),
-    REFRESH_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "RefreshToken is Expired. Please Login again."),
-    REFRESH_TOKEN_NOT_EXIST(HttpStatus.UNAUTHORIZED, "RefreshToken is not exist in database. Please Login again."),
-    REFRESH_TOKEN_NOT_EQUAL(HttpStatus.UNAUTHORIZED, "RefreshToken is not equal in database. Please Login again."),
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "Not found member with your primary key."),
     DECRYPT_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Error during decrypting primary key."),
     JSON_CONVERT_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"Failed to convert Json with Object");
