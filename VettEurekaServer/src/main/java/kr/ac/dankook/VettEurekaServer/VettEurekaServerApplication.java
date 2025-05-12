3d81687c0731fcdf296e68dc9c70ea2665bf4cde
ohchanKyu <okc0202@naver.com>
2025-05-12 21:40:43 +0900

    chore: Modify docker-compose file and properties.

--- VettEurekaServer/src/main/java/kr/ac/dankook/VettEurekaServer/VettEurekaServerApplication.java
+++ VettEurekaServer/src/main/java/kr/ac/dankook/VettEurekaServer/VettEurekaServerApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class VettEurekaServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(VettEurekaServerApplication.class, args);
 	}
-
 }
