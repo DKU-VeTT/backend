d4abdb7d811a5707c651ccc969af0258670ea08b
ohchanKyu <okc0202@naver.com>
2025-05-12 17:34:09 +0900

    feat: Add create vectorDB logic.

--- VettLLMServer/app.py
+++ VettLLMServer/app.py
@@ -1,5 +1,14 @@
 import os
 import re
+import textwrap
+import json
+
+from pinecone import Pinecone
+from langchain_pinecone import PineconeVectorStore
+from langchain_upstage import UpstageEmbeddings
+from langchain_community.document_loaders import Docx2txtLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
 from fastapi import FastAPI, Request, HTTPException, Depends
 from fastapi.responses import PlainTextResponse, JSONResponse
 from dotenv import load_dotenv
@@ -8,8 +17,6 @@ from llm import get_rag_chain
 from llm import get_dictionary_chain
 from llm import get_llm
 from fastapi.middleware.cors import CORSMiddleware
-import textwrap
-import json
 
 load_dotenv()
 
@@ -33,6 +40,24 @@ async def verify_frontend_token(request: Request):
                 "detail": "Invalid FrontEnd token"
             }
         )
+
+
+@app.post("/fetch", response_class=PlainTextResponse)
+async def initial_vector_database(request: Request, _: None = Depends(verify_frontend_token)):
+
+    embedding = UpstageEmbeddings(model="embedding-query")
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1500,
+        chunk_overlap=200
+    )
+    loader = Docx2txtLoader('./data.docx')
+    document_list = loader.load_and_split(text_splitter=text_splitter)
+
+    index_name = 'vett-upstage-index'
+    pinecone_api_key = os.environ.get("PINECONE_API_KEY")
+    pc = Pinecone(api_key=pinecone_api_key)
+    database = PineconeVectorStore.from_documents(document_list, embedding, index_name=index_name)
+    return "Success Initial Vector Database."
 
 
 @app.post("/chat", response_class=PlainTextResponse)
